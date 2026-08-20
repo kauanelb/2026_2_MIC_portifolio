56399944861fd1ae4675ab11b717c5c124c0e923
kauanelb <kauanelb@aluno.ifsc.edu.br>
2026-08-20 11:35:39 -0300

    Experimento com pinos de entrada e chaves Adicionado entrada no pino PD3 com chave, simulação foi atualizada para piscar o led quando a chave está fechada

--- portifolio_mic/aula1_gpio/main.c
+++ portifolio_mic/aula1_gpio/main.c
@@ -11,10 +11,14 @@
 
 int main(void){
 	DDRB |= 0b00100000; //define pino PB5 como saída
+	DDRD &= 0b11110111; //define pino PD3 como entrada
     while(1){
-		PORTB |= 0b00100000; //Atribui nível lógico alto ao pino PB5
-        _delay_ms(100);
-		PORTB &= 0b11011111; //Atribui nível lógico baixo ao pino PB5
-		_delay_ms(900);
-    }
+		if((PIND & 0b00001000) != 0){//Ler valor da chave PD3 segue se estiver em nível lógico alto
+			PORTB |= 0b00100000; //Atribui nível lógico alto ao pino PB5
+			_delay_ms(100);
+			PORTB &= 0b11011111; //Atribui nível lógico baixo ao pino PB5
+			_delay_ms(900);
+			//chave fechada pisca, chave aberta não
+		}
+	}
 }
