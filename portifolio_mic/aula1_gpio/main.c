911df6f97c98faad20a0ea69b547bc87e7e1a803
kauanelb <kauanelb@aluno.ifsc.edu.br>
2026-08-20 11:15:35 -0300

    Código para piscar o led

--- portifolio_mic/aula1_gpio/main.c
+++ portifolio_mic/aula1_gpio/main.c
@@ -5,11 +5,16 @@
  *  Author: kauanelb
  */ 
 
+#define F_CPU 16000000
 #include <xc.h>
+#include "util/delay.h"
 
 int main(void){
-    while(1)
-    {
-        //TODO:: Please write your application code 
+	DDRB |= 0b00100000; //define pino PB5 como saída
+    while(1){
+		PORTB |= 0b00100000; //Atribui nível lógico alto ao pino PB5
+        _delay_ms(100);
+		PORTB &= 0b11011111; //Atribui nível lógico baixo ao pino PB5
+		_delay_ms(900);
     }
 }
