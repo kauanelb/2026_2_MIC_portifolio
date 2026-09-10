1504b56d3d4b76194f01618747a5623a0e33999b
kauanelb <kauane.lb@aluno.ifsc.edu.br>
2026-09-10 09:52:01 -0300

    Aula 5 - Parte 2 - Barra de LEDs na bancada Datapool

--- portifolio_mic/aula5_avrdude/main.c
+++ portifolio_mic/aula5_avrdude/main.c
@@ -9,14 +9,24 @@
 # include <xc.h>
 # include "util/delay.h"
 
-void GPIO_config () {
-	DDRB |= (1 << DDB5 ) ; // Pino PB5 (13) como saida
+void GPIO_incBar () {
+	PORTD = PORTD >> 1;
+	PORTD |= 0b10000000 ;
 }
 
-int main (void) {
+void GPIO_decBar () {
+	PORTD = PORTD << 1;
+}
+
+void GPIO_config () {
+	DDRD = 0xFF ; // pinos usados pelos LEDs como saida
+	PORTD = 0x00 ; // parte com a barra apagada
+}
+
+int main ( void ) {
 	GPIO_config () ;
 	while (1) {
-	PORTB ^= (1 << PORTB5 ) ; // Alterna o estado do LED
-	_delay_ms (500) ;
+		for ( uint8_t i = 0; i < 8; i ++) { GPIO_incBar () ; _delay_ms (200) ; }
+		for ( uint8_t i = 0; i < 8; i ++) { GPIO_decBar () ; _delay_ms (200) ; }
 	}
-}
+}
