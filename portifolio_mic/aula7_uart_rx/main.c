184256878eeadac5fd583e8592e9705419f50107
kauanelb <kauane.lb@aluno.ifsc.edu.br>
2026-09-24 11:22:32 -0300

    Tratamento de recepção UART via interrupção

--- portifolio_mic/aula7_uart_rx/main.c
+++ portifolio_mic/aula7_uart_rx/main.c
@@ -8,10 +8,12 @@
 #define F_CPU 16000000
 #include <xc.h>
 #include "util/delay.h"
+#include "avr/interrupt.h"
 
 void UART_config_rx(uint16_t pBAUD){ 
 	UCSR0A = (0<<U2X0);							//Modo "double speed" desativado, tem relação com o ubrr0
-	UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02); //1 - Habilita o receptor. 2 - Desabilita o transmissor UART0. 3 - pg 162, configurar o tamanho do frame, falar quantos bits vão ser enviados tabela 19.7
+	UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02) //1 - Habilita o receptor. 2 - Desabilita o transmissor UART0. 3 - pg 162, configurar o tamanho do frame, falar quantos bits vão ser enviados tabela 19.7
+			|(1<<RXCIE0); 	//Habilita a interrupção de recepção da UART0 pg 160 19.10.3
 	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)          //Modo assíncrono pg 161 tabela 19.4
 			|(1<<UPM01)|(0<<UPM00)              //Tabela 19.5 pg 162, paridade par
 			|(1<<USBS0)                         //2 bits de stop
@@ -24,22 +26,25 @@ void GPIO_config(){
 	DDRC = (1<<DDC0)|(1<<DDC1);// Porta PC0 e PC1 como saída
 }
 
+ISR(USART_RX_vect){//Interrupção cap 11, pg 49, interrupção 19
+	uint8_t tReceiveByte = UDR0; // 19.10.1 flag de I/O, leitura do buffer UART
+	//Tratamento da mensagem recebida
+	if(tReceiveByte == 'M'){
+		PORTC |= (1<<PORTC0); //Led verde
+		} else{
+		PORTC |= (1<<PORTC1); //Led vermelho
+	}
+	_delay_ms(1);
+	PORTC = 0; //Apaga todos os leds
+}
+
 int main(void)
 {
 	GPIO_config();
 	UART_config_rx(9600);
+	sei();//Habilita interrupções globalmente
     while(1)
     {
-		//19.10.2 bit 7 RXCn
-		while((UCSR0A & (1<<RXC0)) == 0); // Semáforo na flag de recebimento da UART, enquanto nao tiver nada pra ler ele fica esperando
-		uint8_t tReceiveByte = UDR0; // 19.10.1 flag de I/O, leitura do buffer UART
-		//Tratamento da mensagem recebida
-		if(tReceiveByte == 'M'){
-			PORTC |= (1<<PORTC0); //Led verde
-		} else{
-			PORTC |= (1<<PORTC1); //Led vermelho
-		}
-		_delay_ms(1);
-		PORTC = 0; //Apaga todos os leds
+				
     }
 }
